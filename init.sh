1e2e10f94e07a6b0754531fb372f19eb6cf61284
natanaeladit <nata.adit@gmail.com>
2025-01-12 20:29:57 +0800

    add linux bash

---
+++ init.sh
@@ -0,0 +1,8 @@
+# Create a new virtual environment
+python -m venv paligemma-env
+
+# Activate the virtual environment
+source paligemma-env/bin/activate
+
+# Install the required libraries
+pip install -r requirements.txt
